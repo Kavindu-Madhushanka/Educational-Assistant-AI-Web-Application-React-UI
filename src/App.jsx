1190afca5dd9a660cebc0816fef45bc1de47e1fd
Kavindu-Madhushanka <madu10275@gmail.com>
2026-06-23 02:38:10 +0530

    modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
+import FirstLoadPage from "./assets/component/first_page/FirstLoadPage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <dev className="w-full min-h-screen bg-[#0b1116]">
-        <h1 className="text-red-500">AI PROJECT</h1>
-
-        <iframe src="/three.js.html" />
-      </dev>
+      <FirstLoadPage />
     </>
   );
 }
