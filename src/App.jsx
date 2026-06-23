67e399baabde296227e1bc94ed48613a24c94738
Kavindu-Madhushanka <madu10275@gmail.com>
2026-06-23 13:28:12 +0530

    modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import FirstLoadPage from "./assets/component/first_page/FirstLoadPage";
+import LoginPage from "./assets/component/log_page/LoginPage";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
   return (
     <>
-      <FirstLoadPage />
+      {isLoading ? (
+        <FirstLoadPage onComplete={() => setIsLoading(false)} />
+      ) : (
+        <LoginPage />
+      )}
     </>
   );
 }
