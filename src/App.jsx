d4939f4aa5f093f6679e76f81f28a366ec622e8b
Kavindu-Madhushanka <madu10275@gmail.com>
2026-07-02 15:59:28 +0530

    modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import FirstLoadPage from "./assets/component/first_page/FirstLoadPage";
 import LoginPage from "./assets/component/log_page/LoginPage";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Dashboard from "./assets/component/dashboard/Dashboard";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isLoading) {
+      navigate("/loading");
+    }
+  }, [isLoading]);
+
+  const handleLoadingComplete = () => {
+    setIsLoading(false);
+    navigate("/loginpage");
+  };
   return (
     <>
-      {isLoading ? (
-        <FirstLoadPage onComplete={() => setIsLoading(false)} />
-      ) : (
-        <LoginPage />
-      )}
+      <Routes>
+        <Route
+          path="/loading"
+          element={<FirstLoadPage onComplete={handleLoadingComplete} />}
+        />
+        <Route path="/loginpage" element={<LoginPage />} />
+        <Route path="/dashborad" element={<Dashboard />} />
+      </Routes>
     </>
   );
 }
